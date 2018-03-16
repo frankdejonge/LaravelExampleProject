09b052201d3b36739598ccf2ad3de6adfdfded79
Frank de Jonge <info@frenky.net>
2018-03-16 19:16:38 +0100

    Move the commands and events into the namespaced foldr

--- config/eventsauce.php
+++ config/eventsauce.php
@@ -3,6 +3,6 @@
 return [
     'sync_consumers' => [],
     'async_consumers' => [],
-    'definition' => __DIR__.'/../eventsauce/commands-and-events.yml',
-    'output' => __DIR__.'/../eventsauce/commands-and-events.php',
+    'definition' => __DIR__.'/../app/RegisteringMembers/commands-and-events.yml',
+    'output' => __DIR__.'/../app/RegisteringMembers/commands-and-events.php',
 ];
